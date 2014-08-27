f43fd59b669b50a1e1e0de7d90d36e672e79477e
Ryan Lee <ryanlee@zepheira.com>
2014-08-27 12:27:08 -0700

    amend init script to check if pidfile's pid is actually running, behave correctly accordingly

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "ryanlee@zepheira.com"
 license           "Apache 2.0"
 description       "Installs and configures the Akara server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.2.0"
+version           "1.2.1"
 recipe            "akara", "Includes the client recipe to configure a server"
 name              "akara"
 
