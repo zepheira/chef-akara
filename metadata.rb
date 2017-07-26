14d5fdc64d51c41e047189b8cdff4d9a4f273409
Ryan Lee <ryanlee@zepheira.com>
2017-07-26 13:15:08 -0700

    update metadata, supported platforms

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,12 @@ maintainer_email  "ryanlee@zepheira.com"
 license           "Apache 2.0"
 description       "Installs and configures the Akara server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.2.1"
-recipe            "akara", "Includes the client recipe to configure a server"
+chef_version       ">= 12.1" if respond_to?(:chef_version)
+issues_url        "https://github.com/zepheira/chef-akara/issues" if respond_to?(:issues_url)
+source_url        "https://github.com/zepheira/chef-akara" if respond_to?(:source_url)
+supports          "ubuntu", ">= 16.04"
 name              "akara"
+version           "1.2.1"
 
 depends           "python"
 depends           "logrotate"
